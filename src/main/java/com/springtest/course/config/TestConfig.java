4718e970a37825618cc24d4faf6cc40ea77c0986
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2025-01-20 18:05:02 -0300

    Category Entity

--- src/main/java/com/springtest/course/config/TestConfig.java
+++ src/main/java/com/springtest/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.springtest.course.entities.Category;
 import com.springtest.course.entities.Order;
 import com.springtest.course.entities.User;
 import com.springtest.course.entities.enums.OrderStatus;
+import com.springtest.course.repositories.CategoryRepository;
 import com.springtest.course.repositories.OrderRepository;
 import com.springtest.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,5 +41,11 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 }
